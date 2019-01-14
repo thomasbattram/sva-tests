4d74dc389e03dbe86281b23309db776bd574a13e
Thomas Battram <tb13101@bristol.ac.uk>
2019-01-14 14:41:01 +0000

    Added in new code for new sv_list names and added new plots

--- R/post_sva.R
+++ R/post_sva.R
@@ -23,11 +23,15 @@ if (most_var) {
 	mnam <- ""
 }
 
-file <- paste0("data/sv_test_sims", mnam, ".RData")
+# load most var as well for comparison at the end
+load("data/sv_test_simsmost_var.RData")
+sv_list_mv <- sv_list
+file <- "data/sv_test_sims.RData"
 params <- read_delim(paste0("results/sv_test_params_sims", mnam, ".txt"), delim = "\t")
 load(file)
 
 length(sv_list)
+
 
 # ---------------------------------------------------------------
 # Check timings of the results
@@ -38,7 +42,7 @@ all_sv_cont_params <- params %>%
 	dplyr::filter(dat_type == "continuous") %>% 
 	dplyr::filter(n_sv == max(n_sv))
 
-time_plot <- ggplot(all_samp_cont_params, aes(x = n_cpg, y = time_user, colour = as.factor(n_sample))) +
+time_plot <- ggplot(all_sv_cont_params, aes(x = n_cpg, y = time_user, colour = as.factor(n_sample))) +
 	geom_line(aes(linetype = sv_type)) +
 	geom_point() +
 	scale_colour_discrete(name = "n_sample")
@@ -47,12 +51,14 @@ sv_dat_type_params <- params %>%
 	dplyr::filter(n_sample == max(n_sample)) %>%
 	dplyr::filter(sv_type == "smartsva")
 
-sv_time_plot <- ggplot(sv_dat_type_params, aes(x = n_cpg, y = time_user, fill = as.factor(n_sv))) +
-	geom_bar(stat = "identity", position = "dodge") +
-	scale_fill_discrete(name = "n_sv")
+sv_time_plot <- ggplot(sv_dat_type_params, aes(x = n_cpg, y = time_user, colour = as.factor(n_sv))) +
+	geom_point() +
+	geom_line(aes(linetype = dat_type)) +
+	scale_colour_discrete(name = "n_sv")
 
-dat_type_time_plot <- ggplot(sv_dat_type_params, aes(x = n_cpg, y = time_user, fill = as.factor(dat_type))) +
-	geom_bar(stat = "identity", position = "dodge") +
+dat_type_time_plot <- ggplot(sv_dat_type_params, aes(x = n_cpg, y = time_user, colour = as.factor(dat_type))) +
+	geom_point() +
+	geom_line() +
 	scale_fill_discrete(name = "dat_type")
 
 # look at data type (binary or continuous)
@@ -69,20 +75,28 @@ cor(bin, cont) # 0.9982354
 # - n_cpg -- keep 450k
 # - dat_type -- keep continuous
 # - n_sample -- keep max sample size
+# - n_sv -- keep max number of SVs
 
 # testing sv types
+n_cpg <- max(params$n_cpg)
+dat_type <- "continuous"
+n_sample <- max(params$n_sample)
+n_sv <- max(params$n_sv)
 
-sv_type_params <- with(params, which(dat_type == "continuous" 
-					& n_cpg == max(n_cpg)
-					& n_sample == max(n_sample)))
+## sv_list names follow this order: DT_NSV_SVTYPE_NCPG_NSAMP
+list_nam <- with(params, paste("con",
+				 max(n_sv), 
+				 unique(sv_type), 
+				 paste0(round(max(n_cpg)/1000), "k"), 
+				 max(n_sample), 
+				 sep = "_"))
 
-sva_450 <- sv_list[[sv_type_params[1]]]
-smsva_450 <- sv_list[[sv_type_params[2]]]
-params[sv_type_params,]
-str(sv_list[sv_type_params[2]])
+str(sv_list[576])
+sva_450 <- sv_list[[list_nam[2]]]
+smsva_450 <- sv_list[[list_nam[1]]]
 
+# assess the correlation between the 20 SVs produced and see how much variance one explains of the other
 smart_v_normal <- data.frame(sv = 1:20, cor = NA, adj_r2 = NA)
-
 for (j in 1:nrow(smart_v_normal)) {
 	sva_sv <- sva_450$sv[, j]
 	smsva_sv <- smsva_450$sv[, j]
@@ -94,21 +108,25 @@ for (j in 1:nrow(smart_v_normal)) {
 write.table(smart_v_normal, file = paste0("results/smartsva_v_sva_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
 
 # 450k vs lower 
-sv_ncpg_params <- with(params, which(dat_type == "continuous" 
-					& n_sample == max(n_sample) 
-					& sv_type == "smartsva"))
-params[sv_ncpg_params,]
+list_nam <- with(params, paste("con",
+				 max(n_sv), 
+				 "smartsva", 
+				 paste0(round(unique(n_cpg)/1000), "k"), 
+				 max(n_sample), 
+				 sep = "_"))
 
-ncpg_dat <- as.data.frame(matrix(NA, nrow = length(sv_ncpg_params), ncol = 21))
+ncpg_dat <- as.data.frame(matrix(NA, nrow = length(list_nam), ncol = 21))
 colnames(ncpg_dat)[1] <- "n_cpg"
 colnames(ncpg_dat)[2:21] <- paste0("sv", 1:20, "_adjr2")
 
-smsva_450 <- sv_list[[sv_ncpg_params[length(sv_ncpg_params)]]]
+smsva_450 <- sv_list[[list_nam[grep("483k", list_nam)]]]
 svs_450 <- smsva_450$sv
 i=1
-for (i in 1:length(sv_ncpg_params)) {
-	temp_svs <- sv_list[[sv_ncpg_params[i]]]$sv
-	ncpg_dat[i, "n_cpg"] <- params[sv_ncpg_params[i], "n_cpg"]
+for (i in 1:length(list_nam)) {
+	temp_svs <- sv_list[[list_nam[i]]]$sv
+	ncpg <- str_split(list_nam[i], "_")[[1]][4]
+	ncpg <- as.numeric(gsub("k", "", ncpg)) * 1000
+	ncpg_dat[i, "n_cpg"] <- ncpg
 	fom <- as.formula(paste0("svs_450[, j] ~ ", paste(paste0("temp_svs[, ", 1:20, "]"), collapse = " + ")))
 	for (j in 1:ncol(svs_450)) {
 		fit <- lm(fom)
@@ -130,21 +148,26 @@ ncpg_plot <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, so
 write.table(ncpg_dat, file = paste0("results/ncpg_comp_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
 
 # Using 10 SVs, 450k vs lower 
-sv_ncpg_params <- with(params, which(dat_type == "continuous" 
-					& n_sample == max(n_sample) 
-					& sv_type == "smartsva"))
-params[sv_ncpg_params,]
+list_nam <- with(params, paste("con",
+				 max(n_sv), 
+				 "smartsva", 
+				 paste0(round(unique(n_cpg)/1000), "k"), 
+				 max(n_sample), 
+				 sep = "_"))
 
-ncpg_dat <- as.data.frame(matrix(NA, nrow = length(sv_ncpg_params), ncol = 11))
+ncpg_dat <- as.data.frame(matrix(NA, nrow = length(list_nam), ncol = 11))
 colnames(ncpg_dat)[1] <- "n_cpg"
 colnames(ncpg_dat)[2:11] <- paste0("sv", 1:10, "_adjr2")
 
-smsva_450 <- sv_list[[sv_ncpg_params[length(sv_ncpg_params)]]]
+smsva_450 <- sv_list[[list_nam[grep("483k", list_nam)]]]
 svs_450 <- smsva_450$sv[,1:10]
+
 i=1
-for (i in 1:length(sv_ncpg_params)) {
-	temp_svs <- sv_list[[sv_ncpg_params[i]]]$sv[,1:10]
-	ncpg_dat[i, "n_cpg"] <- params[sv_ncpg_params[i], "n_cpg"]
+for (i in 1:length(list_nam)) {
+	temp_svs <- sv_list[[list_nam[i]]]$sv[,1:10]
+	ncpg <- str_split(list_nam[i], "_")[[1]][4]
+	ncpg <- as.numeric(gsub("k", "", ncpg)) * 1000
+	ncpg_dat[i, "n_cpg"] <- ncpg
 	fom <- as.formula(paste0("svs_450[, j] ~ ", paste(paste0("temp_svs[, ", 1:10, "]"), collapse = " + ")))
 	for (j in 1:ncol(svs_450)) {
 		fit <- lm(fom)
@@ -165,22 +188,50 @@ ncpg_plot_10svs <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(
 
 write.table(ncpg_dat, file = paste0("results/ncpg_comp_10_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
 
+## Taking most variable CpGs 
+ncpg_dat <- as.data.frame(matrix(NA, nrow = length(list_nam), ncol = 11))
+colnames(ncpg_dat)[1] <- "n_cpg"
+colnames(ncpg_dat)[2:11] <- paste0("sv", 1:10, "_adjr2")
 
-# continuous vs binary
-sv_dattype_params <- with(params, which(n_cpg == max(n_cpg) 
-					& n_sample == max(n_sample) 
-					& sv_type == "smartsva"))
-params[sv_dattype_params,]
+smsva_450 <- sv_list_mv[[list_nam[grep("483k", list_nam)]]]
+svs_450 <- smsva_450$sv[,1:10]
 
-# sample size changes
-sv_nsamp_params <- with(params, which(dat_type == "continuous" 
-					& n_cpg == max(n_cpg) 
-					& sv_type == "smartsva"))
+i=1
+for (i in 1:length(list_nam)) {
+	temp_svs <- sv_list_mv[[list_nam[i]]]$sv[,1:10]
+	ncpg <- str_split(list_nam[i], "_")[[1]][4]
+	ncpg <- as.numeric(gsub("k", "", ncpg)) * 1000
+	ncpg_dat[i, "n_cpg"] <- ncpg
+	fom <- as.formula(paste0("svs_450[, j] ~ ", paste(paste0("temp_svs[, ", 1:10, "]"), collapse = " + ")))
+	for (j in 1:ncol(svs_450)) {
+		fit <- lm(fom)
+		ncpg_dat[i, j+1] <- summary(fit)$adj.r.squared
+	}
+}
 
-params[sv_nsamp_params,]
+ncpg_dat_mv <- ncpg_dat %>%
+	dplyr::mutate(cpg_subset = "most_variable")
 
-# final plots to summarise it! 
+ncpg_dat <- read_delim("results/ncpg_comp_10_sims.txt", delim = "\t")
+
+ncpg_dat <- ncpg_dat %>%
+	dplyr::mutate(cpg_subset = "random") %>%
+	rbind(ncpg_dat_mv)
+
+plot_res <- ncpg_dat %>%
+	gather(key = sv, value = adj_r2, -n_cpg, -cpg_subset)
+plot_res$sv <- gsub("_adjr2", "", plot_res$sv)
+plot_res$sv <- gsub("sv", "", plot_res$sv)
+
+# plot it!
+rand_v_mv_plot <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, sort(as.numeric(sv))))) +
+	geom_line(aes(linetype = cpg_subset)) +
+	geom_point() +
+	scale_colour_discrete(name = "SV")
+
+
+# final plots to summarise it!
 pdf(paste0("results/sv_plots", mnam, ".pdf"))
-marrangeGrob(list(time_plot, sv_time_plot, dat_type_time_plot, ncpg_plot, ncpg_plot_10svs), ncol = 1, nrow = 1)
+marrangeGrob(list(time_plot, sv_time_plot, ncpg_plot, ncpg_plot_10svs, rand_v_mv_plot), ncol = 1, nrow = 1)
 dev.off()
 
