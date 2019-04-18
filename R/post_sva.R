ef78af59c202b4ff38fcd41ea1bc3735de4c225f
Thomas Battram <tb13101@bristol.ac.uk>
2019-04-18 16:10:10 +0100

    Tidied code

--- R/post_sva.R
+++ R/post_sva.R
@@ -38,7 +38,8 @@ length(sv_list)
 
 all_sv_cont_params <- params %>%
 	dplyr::filter(dat_type == "continuous") %>% 
-	dplyr::filter(n_sv == 20)
+	dplyr::filter(n_sv == 20) %>%
+	dplyr::filter(covs == "none")
 
 time_plot <- ggplot(all_sv_cont_params, aes(x = n_cpg, y = time_user, colour = as.factor(n_sample))) +
 	geom_line(aes(linetype = sv_type)) +
@@ -47,7 +48,8 @@ time_plot <- ggplot(all_sv_cont_params, aes(x = n_cpg, y = time_user, colour = a
 
 sv_dat_type_params <- params %>%
 	dplyr::filter(n_sample == max(n_sample)) %>%
-	dplyr::filter(sv_type == "smartsva")
+	dplyr::filter(sv_type == "smartsva") %>%
+	dplyr::filter(covs == "none")
 
 sv_time_plot <- ggplot(sv_dat_type_params, aes(x = n_cpg, y = time_user, colour = as.factor(n_sv))) +
 	geom_point() +
@@ -60,9 +62,15 @@ dat_type_time_plot <- ggplot(sv_dat_type_params, aes(x = n_cpg, y = time_user, c
 	scale_fill_discrete(name = "dat_type")
 
 # look at data type (binary or continuous)
-bin <- params[params$dat_type == "binary", "time_user"]
-cont <- params[params$dat_type == "continuous", "time_user"]
-cor(bin, cont) # 0.9982354
+bin_dat <- params %>%
+	dplyr::filter(covs == "none") %>%
+	dplyr::filter(n_cpg == max(n_cpg)) %>%
+	dplyr::filter(n_sample == max(n_sample)) %>%
+	dplyr::filter(n_sv < 21)
+
+bin <- bin_dat[bin_dat$dat_type == "binary", "time_user"]
+cont <- bin_dat[bin_dat$dat_type == "continuous", "time_user"]
+cor(bin, cont) # 0.9994115
 
 
 # ---------------------------------------------------------------
@@ -74,12 +82,25 @@ cor(bin, cont) # 0.9982354
 # - dat_type -- keep continuous
 # - n_sample -- keep max sample size
 # - n_sv -- keep max number of SVs
+# - covs -- keep "none"
 
 # testing sv types
 n_cpg <- max(params$n_cpg)
 dat_type <- "continuous"
 n_sample <- max(params$n_sample)
 n_sv <- 20
+covs <- "none"
+
+params_cc <- params %>% 
+	dplyr::filter(n_sample == 100) %>%
+	dplyr::filter(n_sv == 20) %>%
+	dplyr::filter(dat_type == "continuous") %>%
+	dplyr::filter(sv_type == "smartsva")
+
+params <- params %>%
+	dplyr::filter(covs == "none")
+
+names(sv_list)
 
 ## sv_list names follow this order: DT_NSV_SVTYPE_NCPG_NSAMP
 list_nam <- with(params, paste("con",
@@ -133,12 +154,6 @@ pdf("results/smsva_vs_sva_svplots.pdf")
 marrangeGrob(sv_type_plot_list, nrow=4, ncol=5)
 dev.off()
 
-sv_type_plot_list[[j]] <- ggplot(plot_df, aes(x = sva, y = smsva)) +
-		geom_point() + 
-		geom_abline(colour = "red") + 
-		geom_smooth() + 
-		ggtitle(paste0("SV", j))
-
 # 450k vs lower 
 list_nam <- with(params, paste("con",
 				 20, 
@@ -178,47 +193,6 @@ ncpg_plot <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, so
 	scale_colour_discrete(name = "SV")
 
 write.table(ncpg_dat, file = paste0("results/ncpg_comp_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
-
-# Using 10 SVs, 450k vs lower 
-list_nam <- with(params, paste("con",
-				 max(n_sv), 
-				 "smartsva", 
-				 paste0(round(unique(n_cpg)/1000), "k"), 
-				 max(n_sample), 
-				 sep = "_"))
-
-ncpg_dat <- as.data.frame(matrix(NA, nrow = length(list_nam), ncol = 11))
-colnames(ncpg_dat)[1] <- "n_cpg"
-colnames(ncpg_dat)[2:11] <- paste0("sv", 1:10, "_adjr2")
-
-smsva_450 <- sv_list[[list_nam[grep("483k", list_nam)]]]
-svs_450 <- smsva_450$sv[,1:10]
-
-i=1
-for (i in 1:length(list_nam)) {
-	temp_svs <- sv_list[[list_nam[i]]]$sv[,1:10]
-	ncpg <- str_split(list_nam[i], "_")[[1]][4]
-	ncpg <- as.numeric(gsub("k", "", ncpg)) * 1000
-	ncpg_dat[i, "n_cpg"] <- ncpg
-	fom <- as.formula(paste0("svs_450[, j] ~ ", paste(paste0("temp_svs[, ", 1:10, "]"), collapse = " + ")))
-	for (j in 1:ncol(svs_450)) {
-		fit <- lm(fom)
-		ncpg_dat[i, j+1] <- summary(fit)$adj.r.squared
-	}
-}
-
-plot_res <- ncpg_dat %>%
-	gather(key = sv, value = adj_r2, -n_cpg)
-plot_res$sv <- gsub("_adjr2", "", plot_res$sv)
-plot_res$sv <- gsub("sv", "", plot_res$sv)
-
-# plot it!
-ncpg_plot_10svs <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, sort(as.numeric(sv))))) +
-	geom_line() +
-	geom_point() +
-	scale_colour_discrete(name = "SV")
-
-write.table(ncpg_dat, file = paste0("results/ncpg_comp_10_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
 
 # 450k vs lower using 60 SVs
 list_nam <- with(params, paste("con",
@@ -260,6 +234,47 @@ ncpg_plot <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, so
 
 write.table(ncpg_dat, file = paste0("results/ncpg_comp_60_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
 
+
+# Using 10 SVs, 450k vs lower 
+list_nam <- with(params, paste("con",
+				 20, 
+				 "smartsva", 
+				 paste0(round(unique(n_cpg)/1000), "k"), 
+				 max(n_sample), 
+				 sep = "_"))
+
+ncpg_dat <- as.data.frame(matrix(NA, nrow = length(list_nam), ncol = 11))
+colnames(ncpg_dat)[1] <- "n_cpg"
+colnames(ncpg_dat)[2:11] <- paste0("sv", 1:10, "_adjr2")
+
+smsva_450 <- sv_list[[list_nam[grep("483k", list_nam)]]]
+svs_450 <- smsva_450$sv[,1:10]
+
+i=1
+for (i in 1:length(list_nam)) {
+	temp_svs <- sv_list[[list_nam[i]]]$sv[,1:10]
+	ncpg <- str_split(list_nam[i], "_")[[1]][4]
+	ncpg <- as.numeric(gsub("k", "", ncpg)) * 1000
+	ncpg_dat[i, "n_cpg"] <- ncpg
+	fom <- as.formula(paste0("svs_450[, j] ~ ", paste(paste0("temp_svs[, ", 1:10, "]"), collapse = " + ")))
+	for (j in 1:ncol(svs_450)) {
+		fit <- lm(fom)
+		ncpg_dat[i, j+1] <- summary(fit)$adj.r.squared
+	}
+}
+
+plot_res <- ncpg_dat %>%
+	gather(key = sv, value = adj_r2, -n_cpg)
+plot_res$sv <- gsub("_adjr2", "", plot_res$sv)
+plot_res$sv <- gsub("sv", "", plot_res$sv)
+
+# plot it!
+ncpg_plot_10svs <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, sort(as.numeric(sv))))) +
+	geom_line() +
+	geom_point() +
+	scale_colour_discrete(name = "SV")
+
+write.table(ncpg_dat, file = paste0("results/ncpg_comp_10_sims", mnam, ".txt"), quote = F, row.names = F, col.names = T, sep = "\t")
 
 
 ## Taking most variable CpGs 
@@ -305,9 +320,68 @@ rand_v_mv_plot <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(s
 	geom_point() +
 	scale_colour_discrete(name = "SV")
 
+# checking whether adding covs changes anything!!!! 
+# 450k vs lower 
+list_nam <- with(params_cc, paste("con",
+				 20, 
+				 "smartsva", 
+				 paste0(round(unique(n_cpg)/1000), "k"), 
+				 min(n_sample), 
+				 sep = "_"))
+
+list_nam_cc <- paste0(list_nam, "_cc")
+covs <- c("none", "cc")
+
+x=covs[1]
+cc_ncpg_dat <- lapply (covs, function(x) {
+	if (x == "none") {
+		nam <- list_nam
+	} else {
+		nam <- list_nam_cc
+	}
+	ncpg_dat <- as.data.frame(matrix(NA, nrow = length(nam), ncol = 21))
+	colnames(ncpg_dat)[1] <- "n_cpg"
+	colnames(ncpg_dat)[2:21] <- paste0("sv", 1:20, "_adjr2")
+
+	smsva_450 <- sv_list[[nam[grep("483k", nam)]]]
+	svs_450 <- smsva_450$sv
+	i=1
+	for (i in 1:length(nam)) {
+		temp_svs <- sv_list[[nam[i]]]$sv
+		ncpg <- str_split(nam[i], "_")[[1]][4]
+		ncpg <- as.numeric(gsub("k", "", ncpg)) * 1000
+		ncpg_dat[i, "n_cpg"] <- ncpg
+		fom <- as.formula(paste0("svs_450[, j] ~ ", paste(paste0("temp_svs[, ", 1:20, "]"), collapse = " + ")))
+		for (j in 1:ncol(svs_450)) {
+			fit <- lm(fom)
+			ncpg_dat[i, j+1] <- summary(fit)$adj.r.squared
+		}
+	}
+	ncpg_dat$cov <- x
+	return(ncpg_dat)
+
+})
+
+ncpg_dat <- cc_ncpg_dat[[1]]
+p_list <- lapply(cc_ncpg_dat, function(ncpg_dat) {
+	plot_res <- ncpg_dat %>%
+		gather(key = sv, value = adj_r2, -n_cpg, -cov)
+	plot_res$sv <- gsub("_adjr2", "", plot_res$sv)
+	plot_res$sv <- gsub("sv", "", plot_res$sv)
+
+	p <- ggplot(plot_res, aes(x = n_cpg, y = adj_r2, colour = reorder(sv, sort(as.numeric(sv))))) +
+	geom_line() +
+	geom_point() +
+	scale_colour_discrete(name = "SV")
+	return(p)
+})
+
+ggsave("results/checking_covariates_in_model.pdf", plot = marrangeGrob(p_list, nrow = 1, ncol = 2))
+
+save(cc_ncpg_dat, file = "results/checking_models_ncpg_dat.RData")
 
 # final plots to summarise it!
 pdf(paste0("results/sv_plots", mnam, ".pdf"))
-marrangeGrob(list(time_plot, sv_time_plot, ncpg_plot, ncpg_plot_10svs), ncol = 1, nrow = 1)
+marrangeGrob(c(list(time_plot, sv_time_plot, ncpg_plot, ncpg_plot_10svs), ncol = 1, nrow = 1)
 dev.off()
 
--- R/test_sva_simdat.R
+++ R/test_sva_simdat.R
@@ -14,6 +14,7 @@ most_var <- as.logical(args[1])
 ms_dir <- args[2]
 wd <- args[3]
 
+# most_var = FALSE
 # ms_dir <- "/panfs/panasas01/sscm/ms13525/aries-release-v4/data/"
 # wd <- "~/sva_tests/"
 
@@ -25,15 +26,24 @@ TP <- "FOM"
 load(paste0(ms_dir, "samplesheet/data.Robj"))
 samplesheet <- dplyr::filter(samplesheet, time_point == TP) %>%
 	dplyr::filter(is.na(duplicate.rm))
-
+samplesheet$ALN <- as.integer(samplesheet$ALN)
 #load the methylation data
 load(paste0(ms_dir, "betas/data.Robj"))
 meth <- beta[, samplesheet$Sample_Name] #keep the samples that correspond to the time point you're interested in
 rm(beta)
 dim(meth)
 
-mdata <- as.matrix(meth)
-mdata <- mdata[complete.cases(mdata), ]
+phen_dir <- "~/main_project/ALSPAC_EWAS/methyl_variance/phen/"
+# load the covars
+covars <- read_delim(paste0(phen_dir, "FOM/FOM.qcovar"), delim = " ", col_names = F)
+colnames(covars) <- c("ALN", "Sample_Name", "Bcell", "CD4T", "CD8T", "Gran", "Mono", "NK", "age", paste0("PC", 1:10))
+
+pheno <- samplesheet %>%
+	left_join(covars) %>%
+	dplyr::select(one_of(colnames(covars)), BCD_id, BCD_plate, MSA4Plate_id, Slide) %>%
+	dplyr::filter(!is.na(Bcell))
+
+mdata <- meth[complete.cases(meth), ]
 dim(mdata)
 
 # load in the old data if there 
@@ -47,8 +57,7 @@ file <- paste0("data/sv_test_sims", mnam, ".RData")
 
 if (file.exists(file)) {
 	load(file)
-	params <- read_delim(file = paste0("results/sv_test_params_sims", mnam, ".txt"), delim = "\t")
-	params_2 <- params
+	params_2 <- read_delim(file = paste0("results/sv_test_params_sims", mnam, ".txt"), delim = "\t")
 }
 
 # ---------------------------------------------------------------
@@ -59,7 +68,8 @@ sv_type <- c("sva", "smartsva")
 n_cpg <- c(seq(20000, 300000, 20000), nrow(mdata))
 n_samp <- c(seq(100, ncol(mdata), by = 100))
 dat_type <- c("binary", "continuous")
-params <- expand.grid(n_sv = n_sv, sv_type = sv_type, n_cpg = n_cpg, dat_type = dat_type, n_sample = n_samp)
+covs <- c("none", "cc")
+params <- expand.grid(n_sv = n_sv, sv_type = sv_type, n_cpg = n_cpg, dat_type = dat_type, n_sample = n_samp, covs = covs)
 params$time_user <- NA
 params$time_system <- NA
 params$time_elapsed <- NA
@@ -73,20 +83,30 @@ phen <- data.frame(binary = bin, continuous = cont)
 params <- params %>%
 	mutate(include = case_when(sv_type == "smartsva" &
 							   dat_type == "continuous" &
+							   covs == "none" &
 							   n_sv < 21 ~ TRUE, 
 							   sv_type == "sva" & 
 							   n_cpg == max(n_cpg) &
 							   n_sample == max(n_sample) &
+							   covs == "none" &
 							   n_sv < 21 ~ TRUE, 
 							   sv_type == "smartsva" &
 							   dat_type == "binary" &
 							   n_cpg == max(n_cpg) &
 							   n_sample == max(n_sample) & 
+							   covs == "none" &
 							   n_sv < 21 ~ TRUE, 
 							   sv_type == "smartsva" &
 							   dat_type == "continuous" &
-							   n_sample == max(n_sample) ~ TRUE)) %>%
+							   covs == "none" &
+							   n_sample == max(n_sample) ~ TRUE, 
+							   sv_type == "smartsva" & 
+							   n_sample == min(n_sample) &
+							   covs == "cc" &
+							   dat_type == "continuous" &
+							   n_sv == 20 ~ TRUE)) %>%
 	dplyr::filter(include == TRUE)
+
 
 if (exists("params_2")) {
 	temp <- params %>%
@@ -106,7 +126,7 @@ if (exists("params_2")) {
 # ---------------------------------------------------------------
 # run analyses
 # ---------------------------------------------------------------
-
+cc_cov <- c("Bcell", "CD4T", "CD8T", "Gran", "Mono", "NK")
 for (i in 1:nrow(params)) {
 	print(i)
 	if (!is.na(params[i, "time_user"])) {
@@ -118,31 +138,50 @@ for (i in 1:nrow(params)) {
 	dt <- params[i, "dat_type"]
 	nsamp <- params[i, "n_sample"]
 	set.seed(2)
-	samp <- sample(1:ncol(mdata), nsamp)
+	co <- params[i, "covs"]
 
-	if (ncpg != nrow(mdata)) {
-		if (most_var) {
-			var.idx <- order(rowVars(mdata, na.rm=T), decreasing=T)[1:ncpg]	
-		} else {
-			var.idx <- sample(1:nrow(mdata), ncpg)
-		}
-		mdat <- mdata[var.idx, , drop=F]
+	if (co == "none") {
+		temp_mdata <- mdata
 	} else {
-		mdat <- mdata
+		temp_mdata <- mdata[, pheno$Sample_Name]
+	}
+	samp <- sample(1:ncol(temp_mdata), nsamp)
+
+	if (ncpg != nrow(temp_mdata)) {
+		if (most_var) {
+			var.idx <- order(rowVars(temp_mdata, na.rm=T), decreasing=T)[1:ncpg]	
+		} else {
+			var.idx <- sample(1:nrow(temp_mdata), ncpg)
+		}
+		mdat <- temp_mdata[var.idx, , drop=F]
+
+	} else {
+		mdat <- temp_mdata
 	}
 
 	mdat <- mdat[, samp]
 	temp_phen <- phen[samp, ]
 	dt_col <- ifelse(dt == "binary", "binary", "continuous")
 	
-	fom <- as.formula(paste0("~", dt))
+	if (co == "none") {
+		fom <- as.formula(paste0("~", dt))
+		mod0=NULL
+	} else if (co == "cc") {
+		temp_phen <- temp_phen %>%
+			mutate(Sample_Name = pheno[samp, "Sample_Name"]) %>%
+			left_join(pheno)
+		stopifnot(all(temp_phen$Sample_Name %in% colnames(mdat)))
+		fom <- as.formula(paste0("~", paste(c(dt, cc_cov), collapse = " + ")))
+		fom0 <- as.formula(paste0("~", paste(cc_cov, collapse = " + ")))
+		mod0 <- model.matrix(fom0, data = temp_phen)
+	}
 	mod <- model.matrix(fom, data = temp_phen)
-	mod0 <- model.matrix(~1, data = temp_phen)
+
 	ptm <- proc.time()
 	if (svtype == "sva") {
-		svobj <- sva(mdat, mod, mod0=NULL, n.sv = nsv, B = 5)
+		svobj <- sva(mdat, mod, mod0=mod0, n.sv = nsv, B = 5)
 	} else if (svtype == "smartsva") {
-		svobj <- smartsva.cpp(mdat, mod, mod0=NULL, n.sv = nsv, B = 5)
+		svobj <- smartsva.cpp(mdat, mod, mod0=mod0, n.sv = nsv, B = 5)
 	}
 
 	tim <- proc.time() - ptm
@@ -150,7 +189,7 @@ for (i in 1:nrow(params)) {
 	params[i, "time_system"] <- tim[2]
 	params[i, "time_elapsed"] <- tim[3]
 
-	if (nsv != max(params$n_sv)) {
+	if (!nsv %in% c(max(params$n_sv),20)) {
 		next
 	} else {
 	## sv_list names follow this order: DT_NSV_SVTYPE_NCPG_NSAMP
@@ -158,6 +197,7 @@ for (i in 1:nrow(params)) {
 		dt <- substr(dt, 1, 3)
 		ncpg <- paste0(round(ncpg/1000), "k")
 		nam <- paste(dt, nsv, svtype, ncpg, nsamp, sep = "_")
+		if (co == "cc") nam <- paste(nam, "cc", sep = "_")
 		sv_list[[nam]] <- svobj	
 	}
 }
